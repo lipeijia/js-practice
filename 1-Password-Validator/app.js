d1c4901aab23d12b95e2e24be8c134c23242ec11
lipeijia <aoeby13@gmail.com>
2019-12-07 12:19:15 +0800

    First-commit

---
+++ 1-Password-Validator/app.js
@@ -0,0 +1,18 @@
+// Write a isValidPassword function
+// It accepts 2 arguments: password and username
+// Password must:
+//	- be at least 8 characters
+//  - cannot contain spaces
+//  - cannot contain the username
+//Otherwise: false
+
+// isValidPassword('89Fjj1nms', 'dogLuvr');  //true
+// isValidPassword('dogLuvr123!', 'dogLuvr') //false
+// isValidPassword('hello1', 'dogLuvr') //false
+
+function isValidPassword(username, password) {
+    if (password.length >= 8 && password.indexOf() !== -1 && .indexOf(username) !== -1) {
+        return true;
+    }
+    return false;
+}
---
+++ 2-Avg/app.js
@@ -0,0 +1,11 @@
+// Write a function to find the average value in an array of numbers
+//avg([0,50]) //25
+//avg([75,76,80,95,100]) //85.2
+
+function avg(arr) {
+    let total = 0;
+    for(let i = 0; i < arr.length; i++ ) {
+        total += arr[i];
+    }
+    return total / arr.length;
+}
