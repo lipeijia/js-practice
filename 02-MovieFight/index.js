ab1a098a20f08b61b24686ed1897ed7939df56ae
peja <aoeby13@gmail.com>
2020-03-10 11:47:31 +0800

    Extracting Utility Functions

--- 02-MovieFight/index.js
+++ 02-MovieFight/index.js
@@ -10,33 +10,9 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector('input');
 
-// let timeoutId;
-// const onInput = event => {
-//     if (timeoutId) {
-//         clearTimeout(timeoutId);
-//     }
-//     timeoutId = setTimeout( () => {
-//         fetchData(event.target.value);
-//     }, 500);
-// }
-// input.addEventListener('input', onInput);
-// 
-// ----- refact with debounce-----
-
-const debounce = (func, delay = 1000) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout( ()=> {
-            func.apply(null, args);
-        },delay); 
-    };
-};
 
 const onInput = event => {
     fetchData(event.target.value);
-}
+};
 
 input.addEventListener('input', debounce(onInput, 500) );
