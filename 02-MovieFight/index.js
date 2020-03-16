de41b83abef03be50373d12fb715bfe640f42e5c
peja <aoeby13@gmail.com>
2020-03-16 17:00:38 +0800

    Rendering Movie Summary

--- 02-MovieFight/index.js
+++ 02-MovieFight/index.js
@@ -16,13 +16,13 @@ const fetchData = async (searchTerm) => {
 // HTML Generation - root
 const root = document.querySelector('.autocompelete');
 root.innerHTML = `
+<div>
 <label><b> search for a movie </b></label>
+<input class = "input" />
+</div>
 <div class = "dropdown">
-  <div class = "dropdown-trigger">
-  <input class = "input" / >
     <div class = "dropdown-menu">
       <div class = "dropdown-content results"></div>
-    </div>
   </div>
 </div>
 `;
@@ -87,5 +87,41 @@ const movieSelected = async (movie) => {
         }
     });
     console.log(response.data);
+    document.querySelector('#summary').innerHTML = movieDetail(response.data);
     
+};
+
+const movieDetail = (movie) => {
+    return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src = "${movie.Poster}" >
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movie.Title}</h1>
+          <h4>${movie.Genre}</h4>
+          <p>${movie.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movie.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movie.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movie.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movie.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    `
 };
